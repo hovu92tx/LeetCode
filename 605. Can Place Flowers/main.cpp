b4cdb324a95c1fa2343139dbf33530a49a73b848
Vu Ho <hovu92tx@gmail.com>
2024-01-03 16:59:05 -0600

    45801032024

--- 605. Can Place Flowers/main.cpp
+++ 605. Can Place Flowers/main.cpp
@@ -6,5 +6,19 @@ class Solution
 public:
     bool canPlaceFlowers(vector<int> &flowerbed, int n)
     {
+        int counter = 0;
+        // add 2 zero spots at the begin and the end of the array to calculate easier.
+        flowerbed.insert(flowerbed.begin(), 0);
+        flowerbed.push_back(0);
+        // scan the array and find the possible spot
+        for (int x = 1; x < flowerbed.size() - 1; x++)
+        {
+            if (flowerbed[x] == 0 && flowerbed[x + 1] == 0 && flowerbed[x - 1] == 0)
+            {
+                flowerbed[x] = 1;
+                counter += 1;
+            }
+        }
+        return counter >= n;
     }
 };
--- 605. Can Place Flowers/main.py
+++ 605. Can Place Flowers/main.py
@@ -5,3 +5,11 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
+        avai_spot = 0
+        #add 2 zero spots at the begin and the end of the array to calculate easier.
+        flowerbed = [0]+flowerbed+[0]
+        #scan the array and find the possible spot
+        for i in range(1,len(flowerbed)-1):
+            if flowerbed[i]!=1 and flowerbed[i-1]!=1 and flowerbed[i+1]!=1:
+                flowerbed[i] = 1
+                avai_spot+=1
