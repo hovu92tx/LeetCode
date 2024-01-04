f54701d4abd78e34f6dffc60c5f7992b37f07798
Vu Ho <hovu92tx@gmail.com>
2024-01-04 12:04:26 -0600

    120401042024

--- 345. Reverse Vowels Of A String/main.cpp
+++ 345. Reverse Vowels Of A String/main.cpp
@@ -4,6 +4,20 @@ using namespace std;
 class Solution {
 public:
     string reverseVowels(string s) {
-        
+        int index= 0;
+        for(int i=0; i<s.length();i++){
+            if (s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U'){
+                for(int j=(s.length()-index);j>=i;j--){
+                    index+=1;
+                    if(s[j]=='a'||s[j]=='e'||s[j]=='i'||s[j]=='o'||s[j]=='u'||s[j]=='A'||s[j]=='E'||s[j]=='I'||s[j]=='O'||s[j]=='U'){
+                        char temp = s[i];
+                        s[i]=s[j];
+                        s[j]=temp;
+                        break;
+                    }
+                }     
+            }
+        }
+        return s;
     }
 };
--- 345. Reverse Vowels Of A String/main.py
+++ 345. Reverse Vowels Of A String/main.py
@@ -4,4 +4,15 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        
+        vowels = {'a','e','i','o','u','A','E','I','O','U'}
+        s_list = list(s)
+        i, j = 0, len(s) - 1
+
+        while i < j:
+            if s_list[i] in vowels:
+                while s_list[j] not in vowels and i < j:
+                    j -= 1
+                s_list[i], s_list[j] = s_list[j], s_list[i]
+                j -= 1
+            i += 1
+        return ''.join(s_list)
